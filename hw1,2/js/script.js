10dae20534b66d2c202eec62d322b0d3acee2ae7
Olizar <punchukv@gmail.com>
2021-05-28 19:50:36 +0300

    First commit

---
+++ hw1,2/js/script.js
@@ -0,0 +1,190 @@
+//TASK 1
+// This is an in-line comment.
+/* This is a
+multi-line comment */
+
+
+
+//TASK 2
+var myName;
+
+
+
+//TASK 3
+// Setup
+var a;
+
+// Only change code below this line
+a = 7;
+
+
+
+//TASK 4
+// Setup
+var a;
+a = 7;
+var b;
+
+// Only change code below this line
+b = a;
+
+
+
+//TASK 5
+var a = 9;
+
+
+
+//TASK 6
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+
+
+//TASK 7
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+
+
+//TASK 8
+var sum = 10 + 10;
+
+
+
+//TASK 9
+var difference = 45 - 33;
+
+
+
+//TASK 10
+var product = 8 * 10;
+
+
+
+//TASK 11
+var quotient = 66 / 33;
+
+
+
+//TASK 12
+var myVar = 87;
+
+// Only change code below this line
+myVar++;
+
+
+
+//TASK 13
+var myVar = 11;
+
+// Only change code below this line
+myVar--;
+
+
+
+//TASK 14
+var ourDecimal = 5.7;
+
+// Only change code below this line
+var myDecimal = 5.7;
+
+
+
+//TASK 15
+var product = 2.0 * 2.5;
+
+
+
+//TASK 16
+var quotient = 4.4 / 2.0; // Change this line
+
+
+
+//TASK 17
+// Only change code below this line
+
+var remainder = 11 % 3;
+
+
+
+//TASK 18
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+
+
+//TASK 19
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+
+
+//TASK 20
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+
+
+//TASK 21
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+
+
+//TASK 22
+var myFirstName = "Vladyslav";
+var myLastName = "Olizarenko";
+
+
+
+//TASK 23
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+
+
+//TASK 24
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+
+
+//TASK 25
+var myStr = 'FirstLine\n\t\\SecondLine\nThirdLine'; // Change this line
